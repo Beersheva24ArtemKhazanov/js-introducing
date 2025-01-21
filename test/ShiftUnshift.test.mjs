64c466583cc3fd038783cc3dc8700b5e10418647
Artem Khazanov <tulenka04@gmail.com>
2025-01-21 17:29:15 +0200

    added tests for shiftUnshift

--- test/ShiftUnshift.test.mjs
+++ test/ShiftUnshift.test.mjs
@@ -9,6 +9,23 @@ describe("shift - unshift test cases", () => {
         const unshiftedSource = stringUnshift(shiftedSource, shift);
         expect(unshiftedSource).toBe(source);
     })
+    it("shift is a string containing number", () => {
+        expect(stringShift(123, "1")).toBe("234")
+    })
+    it("alternative flow for shift that is not a number", () => {
+        expect(stringShift("123", "ab")).toBe("123");
+        expect(stringShift("123", NaN)).toBe("123");
+        expect(stringShift("123",)).toBe("123")
+    })
+    it("shifting number", () => {
+        expect(parseInt(stringShift(123,1))).toBe(234)
+    }) 
+    it("shifting undefined", () => {
+        expect(stringShift(undefined, 1)).toBeUndefined()
+    }) 
+    it("shift null", () => {
+        expect(stringShift(null, 1)).toBeNull()
+    })
     it("alternative flow for shift negative", () => {
         expect(stringShift("123", -4)).toBe("123")
     })
