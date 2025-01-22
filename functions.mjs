86b6ba402d9426c89fb0e309cc333a7accb00936
Artem Khazanov <tulenka04@gmail.com>
2025-01-22 14:18:47 +0200

    init branch

---
+++ functions.mjs
@@ -0,0 +1,16 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+sum.a = 2;
+sum(10, 10);
+sum.call({x : 5}, 10, 10);
+
+const point = {
+    x : 5,
+    y : 10
+};
+function displayPoint(z) {
+    console.log(`x: ${this.x}; y= ${this.y}; z: ${z}`);
+}
+const display = displayPoint.bind(point);
+display(40);
