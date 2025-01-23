c509a3b3c2dd44f972cdc9f1ce451e3bdf58c896
Artem Khazanov <tulenka04@gmail.com>
2025-01-23 15:16:52 +0200

    demo of spread operator in tests

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,59 +1,21 @@
 import { describe, it, expect } from "vitest";
-import { getOccurencesObject } from "../arrays.mjs";
 
-describe("getOccurences test cases", () => {
-    it("regular test", () => {
-        let expectedObj = {
-            "a" : 2,
-            "b" : 2,
-            "c" : 1
-        };
-        expect(getOccurencesObject("abcab")).toEqual(expectedObj);
+describe("meaning of spread operator for arrays and arguments", () => {
+    it("finding maximal number from array", () => {
+        const numbers = [1, 2, 3, 4];
+        expect(Math.max(...numbers)).toBe(4);
     })
-    it("many string test", () => {
-        let expectedObj = {
-            "a" : 4,
-            "b" : 2,
-            "c" : 3,
-            "3" : 1,
-            "2" : 1,
-            "1" : 1,
-            "5" : 1,
-            "6" : 1,
-            "d" : 2,
-            "8" : 1
-        };
-        expect(getOccurencesObject("abc321cab56acdad8")).toEqual(expectedObj);
+    it("pushing one array to another", () => {
+        const array1 = [1, 2];
+        const array2 = [3, 4];
+        const expected = [1, 2, 3, 4];
+        array1.push(...array2);
+        expect(array1).toEqual(expected);
     })
-    it("string is numbers", () => {
-        let expectedObj = {
-            "2" : 1,
-            "5" : 2,
-            "6" : 2,
-            "7" : 1
-        };
-        expect(getOccurencesObject(256756)).toEqual(expectedObj);
-    })
-    it("string has spaces", () => {
-        let expectedObj = {
-            "a" : 4,
-            "b" : 2,
-            "c" : 3,
-            "3" : 1,
-            "2" : 1,
-            " " : 3,
-            "1" : 1,
-            "5" : 1,
-            "6" : 1,
-            "d" : 2,
-            "8" : 1
-        };
-        expect(getOccurencesObject("abc32 1cab56a cda d8")).toEqual(expectedObj);
-    })
-    it("string is null", () => {
-        expect(getOccurencesObject(null)).toEqual("Can't get occurences");
-    })
-    it("string is undefiend", () => {
-        expect(getOccurencesObject()).toEqual("Can't get occurences");
+    it("copying arrays using spread operator", () => {
+        const array1 = [1, 2];
+        const array2 = [...array1];
+        expect(array2).not.toBe(array1);
+        expect(array2).toEqual(array1);
     })
 })
