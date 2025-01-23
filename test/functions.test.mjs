14d605233b0a008f8e4eb027d76518de01476191
Artem Khazanov <tulenka04@gmail.com>
2025-01-23 14:08:42 +0200

    edit tests

--- test/functions.test.mjs
+++ test/functions.test.mjs
@@ -2,18 +2,22 @@ import { it, describe, expect, test } from "vitest";
 import { myBind } from "../functions.mjs";
 
 describe("myBind test cases according ti rhe TODO comments", () => {
-    const point = {x: 3, y: 4};
-    function sumArguments(num1 = 0, num2 = 0) {
-        return this.x + this.y + num1 + num2;
+    const point = {x:3, y:4}; 
+    const dimensions = {x:3, y:4, z:30};
+    point.sumArguments
+    function sumArguments(num3=0, num4=0) {
+        return this.x + this.y + num3 + num4;
     }
     sumArguments.bind = myBind;
-    const func = sumArguments.bind(point);
+    sumArguments.toString = function() {return this.name}
+    console.log(`sumArguments string: ${sumArguments}`)
+    const fun = sumArguments.bind(dimensions);
     it("no additional parameters passed", () => {
-        expect(func()).toBe(7);
+        
+        expect(fun()).toBe(7);
     })
-
-    it(" all parameters are passed through", () => {
-        expect(func(10, 20)).toBe(37);
+    it(" all parameters are passed ", () => {
+        expect(fun(10, 20)).toBe(37)
     })
 
 })
