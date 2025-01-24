8fb4f012990fc75b655db6f573c457683d937733
Artem Khazanov <tulenka04@gmail.com>
2025-01-24 17:21:03 +0200

    methods unshift, push, splice

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -19,3 +19,25 @@ describe("meaning of spread operator for arrays and arguments", () => {
         expect(array2).toEqual(array1);
     })
 })
+
+describe("inserting new elements in array", () => {
+    const inseredNumbers = [-10, -20];
+    it("inserting at begining of array", () => {
+        const ar = [1, 2];
+        const expected = [-10, -20, 1, 2];
+        ar.unshift(...inseredNumbers);
+        expect(ar).toEqual(expected);
+    })
+    it("adding new numbers at end of array", () => {
+        const ar = [1, 2];
+        const expected = [ 1, 2,-10, -20];
+        ar.push(...inseredNumbers);
+        expect(ar).toEqual(expected);
+    })
+    it("inserting new number at middle of array", () => {
+        const ar = [1, 2];
+        const expected = [ 1, -10, -20, 2];
+        ar.splice(1,0,...inseredNumbers)
+        expect(ar).toEqual(expected);
+    })
+})
