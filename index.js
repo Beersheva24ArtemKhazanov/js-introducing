88982864afed4c2b74361d5b66f0c22b48a4fa9d
Artem Khazanov <tulenka04@gmail.com>
2025-01-30 10:59:01 +0200

    init commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log("Hello World");
-let a = 10;
-a *=2;
+func2();
+func1();
+func3();
+function func1() {
+    a = 10;
+}
+
+function func2() {
+    while (a < 3) {
+        console.log(a++);
+    }
+}
+function func3() {
+    
+        console.log(`a=${a}`);
+}
+var a = 0;
